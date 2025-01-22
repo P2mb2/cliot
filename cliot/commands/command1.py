4528a904c1f1c170170d87b88a66a400c2e7b467
João Cardoso <Joao.Cardoso01@vodafone.com>
2025-01-22 09:09:24 +0000

    first code

--- cliot/commands/command1.py
+++ cliot/commands/command1.py
@@ -2,7 +2,7 @@ import click
 
 
 @click.command()
-@click.option('--name', prompt='Your name', help='The person to greet.')
+@click.option("--name", prompt="Your name", help="The person to greet.")
 def command(name):
     """A simple command that greets NAME."""
     click.echo(f"Hello, {name}!")
